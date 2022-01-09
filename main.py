3f5959227a54e70222d12718af469bf0baa67f21
Radeox <dawid.weglarz95@gmail.com>
2022-01-09 01:15:34 +0100

    Removed selenium

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
+import hashlib
 import time
 from subprocess import call
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from telegram.ext import CallbackContext, Updater
 
 from settings import TOKEN, URL
 
-# Initialize the webdriver
-firefox_options = Options()
-firefox_options.add_argument("--headless")
-browser = webdriver.Firefox(options=firefox_options)
-
-
-def get_link() -> str:
-    global browser
-
-    browser.get(URL)
-    time.sleep(10)
-    return browser.find_elements(By.TAG_NAME, "img")[0].get_attribute("src")
-
-
-def save_pic(image_url: str, filename: str) -> None:
-    img_data = requests.get(image_url).content
-    with open(filename, 'wb') as handler:
-        handler.write(img_data)
-
 
 def bot_loop(context: CallbackContext):
     counter = 0
-    old_pic = pic = get_link()
-    save_pic(pic, f"{counter}.jpg")
+    img_data = requests.get(URL).content
+    old_pic = hashlib.sha256(img_data).hexdigest()
 
     # Collect pics
     while counter < 10:
-        pic = get_link()
-        if pic != old_pic:
-            counter += 1
-            old_pic = pic
-            save_pic(pic, f"{counter}.jpg")
+        img_data = requests.get(URL).content
+        new_pic = hashlib.sha256(img_data).hexdigest()
+
+        if old_pic != new_pic:
+            with open(f"{counter}.jpg", 'wb') as f:
+                f.write(img_data)
+                counter += 1
+
+        time.sleep(15)
 
     # Make GIF
     call(["convert", "-delay", "100", "-loop", "1", "*.jpg", "final.gif"])
