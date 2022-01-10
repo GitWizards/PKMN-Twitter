b39dd26ef3e3387828d96059246d0fbeb3df51d4
Radeox <dawid.weglarz95@gmail.com>
2022-01-10 23:35:17 +0100

    Changed loop logic since sometimes they overlap and crash

--- main.py
+++ main.py
@@ -9,35 +9,35 @@ from settings import TOKEN, URL
 
 
 def bot_loop(context: CallbackContext):
-    counter = 0
-    img_data = requests.get(URL).content
-    old_pic = hashlib.sha256(img_data).hexdigest()
+    while True:
+        counter = 0
+        old_pic_hash = ""
 
-    # Collect pics
-    while counter < 10:
-        img_data = requests.get(URL).content
-        new_pic = hashlib.sha256(img_data).hexdigest()
+        # Collect pics
+        while counter < 10:
+            img_data = requests.get(URL).content
+            new_pic_hash = hashlib.sha256(img_data).hexdigest()
 
-        if old_pic != new_pic:
-            old_pic = new_pic
-            with open(f"{counter}.jpg", 'wb') as f:
-                f.write(img_data)
-                counter += 1
+            if old_pic_hash != new_pic_hash:
+                old_pic_hash = new_pic_hash
+                with open(f"{counter}.jpg", 'wb') as f:
+                    f.write(img_data)
+                    counter += 1
 
-        time.sleep(5)
+            time.sleep(15)
 
-    # Make GIF
-    call(["convert", "-delay", "75", "-loop", "1", "*.jpg", "final.gif"])
+        # Make GIF
+        call(["convert", "-delay", "75", "-loop", "1", "*.jpg", "final.gif"])
 
-    # Send GIF to channel
-    with open('final.gif', 'rb') as gif:
-        context.bot.send_animation(chat_id="@pkmn_twitter", animation=gif)
+        # Send GIF to channel
+        with open('final.gif', 'rb') as gif:
+            context.bot.send_animation(chat_id="@radeox", animation=gif)
 
 
 def main():
     print("Starting bot...")
     updater = Updater(TOKEN, use_context=True)
-    updater.job_queue.run_repeating(bot_loop, interval=160, first=0)
+    updater.job_queue.run_once(bot_loop, 0)
     updater.start_polling()
     updater.idle()
 
